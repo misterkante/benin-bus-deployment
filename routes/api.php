94a2ee978d32cd0c8d73b169e3c206370e6f1dd0
Obed ADIFON <Obed ADIFON>
2025-01-27 09:10:21 +0100

    ajout de la route menant vers la fonction affichant les trajets de prix zero

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,6 @@ Route::apiResource('trajet', TrajetController::class);
 
 // Route ajoutée par Obed pour enregistrer automatiquement les villes dans contenues dans la chaîne de caractère passée en paramêtre.
 Route::post('/ligneAndTrajets', [LigneController::class, 'createLigne']);
-Route::post('/ligneAndTrajets', [LigneController::class, 'createLigne']);
+Route::get('/trajets-zero-prix', [TrajetController::class, 'getTrajetsWithZeroPrice']);
 
 require __DIR__ . '/auth.php';
